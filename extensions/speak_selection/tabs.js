f051ce5caabdca59f3c41a91604467597db60ef1
Thomas Tai <thomastai@renaelectronics.com>
2020-12-30 13:13:15 -0500

    Add comments on loadContentScriptInAllTabs()

--- extensions/speak_selection/tabs.js
+++ extensions/speak_selection/tabs.js
@@ -17,17 +17,23 @@ function sendKeyToAllTabs(keyStr) {
   });
 }
 
+/** Chrome does not load content scripts into matching pages at extension load 
+ * (which includes extension updates, not just initial load). We need to
+ * explicitly inject the contect_scripts into the existing tabs at load time.
+ * For the newly created tab after the load time, the manifest.json is used to
+ * specify which content_script will be loaded.
+ */
 function loadContentScriptInAllTabs() {
-  chrome.windows.getAll({'populate': true}, function(windows) {
+  chrome.windows.getAll({ 'populate': true }, function (windows) {
     for (var i = 0; i < windows.length; i++) {
       var tabs = windows[i].tabs;
       for (var j = 0; j < tabs.length; j++) {
         chrome.tabs.executeScript(
-            tabs[j].id,
-            {file: 'keycodes.js', allFrames: true});
+          tabs[j].id,
+          { file: 'keycodes.js', allFrames: true });
         chrome.tabs.executeScript(
-            tabs[j].id,
-            {file: 'content_script.js', allFrames: true});
+          tabs[j].id,
+          { file: 'content_script.js', allFrames: true });
       }
     }
   });
